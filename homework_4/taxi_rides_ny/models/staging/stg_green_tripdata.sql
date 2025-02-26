abb7d77fa7b5756fc6f6a2aa8411f3250b76feea
Konstantinos Soufleros <soufleros.kostas@gmail.com>
2025-02-26 17:18:23 +0000

    running

--- homework_4/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ homework_4/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -7,11 +7,10 @@
 with tripdata as 
 (
   select *,
-    row_number() over(partition by cast(vendorid as STRING), cast(lpep_pickup_datetime as TIMESTAMP)) as rn
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
   from {{ source('staging','green_tripdata') }}
   where vendorid is not null 
 )
-
 select
     -- identifiers
     {{ dbt_utils.generate_surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
@@ -21,14 +20,14 @@ select
     {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dropoff_locationid,
     
     -- timestamps
-    SAFE_CAST(lpep_dropoff_datetime AS TIMESTAMP) AS dropoff_datetime,
-    SAFE_CAST(lpep_pickup_datetime AS TIMESTAMP) AS pickup_datetime,
-
+    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    
     -- trip info
     store_and_fwd_flag,
-    SAFE_CAST(passenger_count AS INT64) AS passenger_count,
+    {{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} as passenger_count,
     cast(trip_distance as numeric) as trip_distance,
-    SAFE_CAST(trip_type AS INT64) AS trip_type,
+    {{ dbt.safe_cast("trip_type", api.Column.translate_type("integer")) }} as trip_type,
 
     -- payment info
     cast(fare_amount as numeric) as fare_amount,
@@ -39,15 +38,11 @@ select
     cast(ehail_fee as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
-    coalesce(safe_cast(payment_type as INT64), 0) as payment_type,
-    {{ get_payment_type_description("payment_type") }} as payment_type_description,
-    cast(congestion_surcharge as numeric) as congestion_surcharge,
-
-    -- Add Service Type
-    'Green' AS service_type  
-
+    coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type,
+    {{ get_payment_type_description("payment_type") }} as payment_type_description
 from tripdata
 where rn = 1
+
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
 {% if var('is_test_run', default=true) %}
